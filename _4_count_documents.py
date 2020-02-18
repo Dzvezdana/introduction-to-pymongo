3a140d3c2d922b357528d3f170ea8cb9a94ac7ba
Alexia <alexiawpy@gmail.com>
2020-02-18 16:38:26 +0100

    #4 finished exercise 4

--- _4_count_documents.py
+++ _4_count_documents.py
@@ -4,51 +4,66 @@ from pymongo import DESCENDING
 client = create_connection()
 
 with client:
-    db = client.testdb
+    db = client.test_database
 
     '''
     Exercise 1
     Count the number of documents in the collection
     '''
-    # n_employee_data = # Add code here
+    # n_employee_data = db.employees.count()
     # print("There are {} employees".format(n_employee_data))
+    # 10
     # print("\n")
 
     '''
     Exercise 2
     Find employees with salary higher than 45000 euros
     '''
-    # expensive_employees = # Add code here
+    # expensive_employees = db.employees.find({'salary':{'$gt': 45000}})
     # for employee in expensive_employees:
     #     print('{0} {1}'.format(
     #         employee['name'],
     #         employee['salary']))
     # print("\n")
+    # Jane 52692
+    # Daniel 65117
+    # Emma 340000
+
 
     '''
     Exercise 3
     Select only the _id and name fields from the returned documents
     '''
-    # employee_data = # Add code here
+    # employee_data = db.employees.find({}, {'name': 1, 'name': 1}) #
     # for employee in employee_data:
     #     print(employee)
-    # print("\n")
 
     '''
     Exercise 4
     Sort the employee using their salary in descending way
     '''
-    # employee_data = # Add code here
+    # employee_data = db.employees.find().sort('salary', -1)
     # for employee in employee_data:
     #     print('{0} {1}'.format(
     #         employee['name'],
     #         employee['salary'])
     #     )
+# Emma 340000
+# Daniel 65117
+# Jane 52692
+# Olivia 41700
+# Christopher 22700
+# Isabella 19600
+# Gonzalez 13000
+# William 10000
+# Anthony 8500
+# Mike 1600
+
 
     '''
     Exercise 5
     Skip the first 2 documents and then print the next 3 results
     '''
-    # employee_data = # Add code here
-    # for employee in employee_data:
-    #     print('{0}: {1}'.format(employee['name'], employee['salary']))
+    employee_data = db.employees.find(skip=2, limit=3)
+    for employee in employee_data:
+        print('{0}: {1}'.format(employee['name'], employee['salary']))
